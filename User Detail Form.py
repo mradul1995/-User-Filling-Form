8b6371f491016df5c25ae7c95d195be9a29ee6ed
Mradul Kumar Chaudhary <147618309+mradul1995@users.noreply.github.com>
2024-07-18 19:59:26 +0530

    18-07-2024
    Short user details form in python.

---
+++ User Detail Form.py
@@ -0,0 +1,19 @@
+name = input("Enter your name: ")
+
+print(f"Hello {name}")
+
+age = input("Enter your age: ")
+
+print("You are younger")
+
+
+mobile = input ("Enter your mobile number: ")
+
+if ("mobile in int"):
+    print("Your mobile number is correct")
+else:
+    print("Your mobile number is incorrect")
+
+
+
+print("Thanks for sharing details")
